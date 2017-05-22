364c1a943ce11f37b9d73224230f1e6d2268135d
burcucan <burcucan99@gmail.com>
2017-05-22 11:14:37 +0300

    plots for accuracy thresholds

--- object_label_classifier/matlab scripts/test.m
+++ object_label_classifier/matlab scripts/test.m
@@ -8,8 +8,20 @@ function probabilities = test()
     addpath('..\..\shared\helper_scripts');
     displayPerformance(test_labels, class_names, probabilities);
     displayPerformanceSingleLabel(test_labels, labels);
-    for i=1:10
+    cutoff_results = zeros(2, 100);
+    for i=1:100
         probs_copy = probabilities;
-        displayPerformanceWithCutoff(test_labels, class_names, probs_copy, i/9);
+        [TP, FP] = displayPerformanceWithCutoff(test_labels, class_names, probs_copy, i/100);
+        cutoff_results(1, i) = TP;
+        cutoff_results(2, i) = FP;
     end
+    X = 1:100;
+    X = X/100;
+    figure;
+    xlabel("Thresholds for Predictions");
+    hold on;
+    plot(X, cutoff_results(1,:));
+    plot(X, cutoff_results(2,:));
+    legend({'True Positives', 'False Positives'});
+    hold off;
 end
